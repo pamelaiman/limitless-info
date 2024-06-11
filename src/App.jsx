d9a8fdc5f9589210d6e9dcc05b49e2671a291469
Pamela Iman <pamelariman@gmail.com>
2024-06-11 15:02:19 +0100

    previous html project converted into react via vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import './App.css'
 
-function App() {
+function App(){
 
   return (
       <div>
-      <h1>Limitless</h1>
+    <h1>LIMITLESS (2011)</h1>
+    <h3>Directed by Neil Burger</h3>
+    <p>`Limitless` is a thrilling science fiction film that follows Eddie Morra, a struggling writer whose life takes a
+      dramatic turn when he discovers NZT-48, a revolutionary drug that unlocks the full potential of his brain. As
+      Eddies mental capabilities soar, he achieves unprecedented success, but he soon discovers that the drug comes
+      with
+      dangerous and deadly side effects. With his newfound intelligence, Eddie must navigate a treacherous path filled
+      with powerful enemies and high stakes, all while trying to uncover the truth behind NZT-48 and secure his future.
+    </p>
+    <p>Rated 7.4 out of 10 on <a target="_blank" href="https://www.imdb.com/title/tt1219289/">IMDB!</a></p>
+    <iframe width="560" height="315"
+      src="https://www.youtube.com/embed/5Y2JMgCa62s?si=Io7eSYC39WGpVm-v&amp;controls=0&amp;start=1"
+      title="YouTube video player" frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <p className="quote"> `I was blind, but now I see.` - Eddie Morra (played by Bradley Cooper)</p>
+    <img src='./limitless.png' alt="Image of man on laptop with letters and numbers around him" />
       </div>
   )
 }
